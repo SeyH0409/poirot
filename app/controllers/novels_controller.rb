f64e79fd0433bdf383bdbac3c5c69ef49bbc03e5
SeyH0409 <199894sei@gmail.com>
2021-05-17 01:57:50 +0900

    表示変更

--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -1,5 +1,5 @@
 class NovelsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @novels = Novel.all.order("created_at DESC")
   end
