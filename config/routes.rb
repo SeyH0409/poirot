e07cf69bba57d4fe8e1c521eaa8c49d57a014e30
SeyH0409 <199894sei@gmail.com>
2021-06-05 21:07:53 +0900

    編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "novels#index"
-  resources :novels, only: [:index, :new, :create, :show, :destroy] do
+  resources :novels do
     resources :tips, only: [:create, :index]
   end
 end
